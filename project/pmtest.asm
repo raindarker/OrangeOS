1df23c7b5c801f47a08cc76916ef38c3ac1f8c12
sphantix <sphantix@gmail.com>
2017-07-16 20:55:41 +0800

    Protect Mode | Add dos bootloader
    Signed-off-by: sphantix <sphantix@gmail.com>

--- project/pmtest.asm
+++ project/pmtest.asm
@@ -10,8 +10,8 @@ org	0100h
 
 [SECTION .gdt]
 ; GDT
-;                              段基址,      段界限     , 属性
-LABEL_GDT:	   Descriptor       0,                0, 0           ; 空描述符
+;                              段基址,       段界限     , 属性
+LABEL_GDT:	       Descriptor       0,                0, 0           ; 空描述符
 LABEL_DESC_CODE32: Descriptor       0, SegCode32Len - 1, DA_C + DA_32; 非一致代码段
 LABEL_DESC_VIDEO:  Descriptor 0B8000h,           0ffffh, DA_DRW	     ; 显存首地址
 ; GDT 结束
@@ -22,7 +22,7 @@ GdtPtr		dw	GdtLen - 1	; GDT界限
 
 ; GDT 选择子
 SelectorCode32		equ	LABEL_DESC_CODE32	- LABEL_GDT
-SelectorVideo		equ	LABEL_DESC_VIDEO	- LABEL_GDT
+SelectorVideo		  equ	LABEL_DESC_VIDEO	- LABEL_GDT
 ; END of [SECTION .gdt]
 
 [SECTION .s16]
