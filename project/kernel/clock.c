66ec1e6bb6bc97bb85b0dd802caf80743764ae45
hangxu <hangxu@antiy.cn>
2017-07-31 20:38:07 +0800

    Process | Switch between two processes

--- project/kernel/clock.c
+++ project/kernel/clock.c
@@ -6,8 +6,15 @@
  */
 
 #include "proto.h"
+#include "proc.h"
 
+extern process_t process_table[NR_TASKS];
+extern process_t* process_ready;
 
 void clock_handler(int irq) {
     disp_str("#");
+    process_ready++;
+    if (process_ready >= process_table + NR_TASKS) {
+        process_ready = process_table;
+    }
 }
