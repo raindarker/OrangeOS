599514e7317fb03a331a64ad3b4a2685f5ea4e67
hangxu <hangxu@antiy.cn>
2017-08-16 19:58:17 +0800

    IO | Enable enter key & backspace key

--- project/kernel/console.c
+++ project/kernel/console.c
@@ -20,22 +20,62 @@ int is_current_console(console_t* console) {
 }
 
 static void set_cursor(unsigned int position) {
-	disable_interrupt();
-	out_byte(CRTC_ADDR_REG, CURSOR_H);
-	out_byte(CRTC_DATA_REG, (position >> 8) & 0xFF);
-	out_byte(CRTC_ADDR_REG, CURSOR_L);
-	out_byte(CRTC_DATA_REG, position & 0xFF);
-	enable_interrupt();
+    disable_interrupt();
+    out_byte(CRTC_ADDR_REG, CURSOR_H);
+    out_byte(CRTC_DATA_REG, (position >> 8) & 0xFF);
+    out_byte(CRTC_ADDR_REG, CURSOR_L);
+    out_byte(CRTC_DATA_REG, position & 0xFF);
+    enable_interrupt();
+}
+
+static void set_video_start_addr(u32 addr) {
+    disable_interrupt();
+    out_byte(CRTC_ADDR_REG, START_ADDR_H);
+    out_byte(CRTC_DATA_REG, (addr >> 8) & 0xFF);
+    out_byte(CRTC_ADDR_REG, START_ADDR_L);
+    out_byte(CRTC_DATA_REG, addr & 0xFF);
+    enable_interrupt();
+}
+
+static void flush(console_t* console) {
+    set_cursor(console->cursor);
+    set_video_start_addr(console->current_start_addr);
 }
 
 void out_char(console_t* console, char c) {
     u8* vmem = (u8*)(V_MEM_BASE + console->cursor * 2);
 
-    *vmem++ = c;
-    *vmem++ = DEFAULT_CHAR_COLOR;
-    console->cursor++;
+    switch(c) {
+    case '\n':
+        if (console->cursor < console->original_addr +
+            console->v_mem_limit - SCREEN_WIDTH) {
+            console->cursor = console->original_addr + SCREEN_WIDTH *
+                ((console->cursor - console->original_addr) /
+                 SCREEN_WIDTH + 1);
+        }
+        break;
+    case '\b':
+        if (console->cursor > console->original_addr) {
+            console->cursor--;
+            *(vmem - 2) = ' ';
+            *(vmem - 1) = DEFAULT_CHAR_COLOR;
+        }
+        break;
+    default:
+        if (console->cursor <
+            console->original_addr + console->v_mem_limit - 1) {
+            *vmem++ = c;
+            *vmem++ = DEFAULT_CHAR_COLOR;
+            console->cursor++;
+        }
+        break;
+    }
 
-    set_cursor(console->cursor);
+    while (console->cursor >= console->current_start_addr + SCREEN_SIZE) {
+        scroll_screen(console, SCR_DN);
+    }
+
+    flush(console);
 }
 
 void init_screen(tty_t* tty) {
@@ -55,20 +95,11 @@ void init_screen(tty_t* tty) {
         tty->console->cursor = g_disp_pos / 2;
         g_disp_pos = 0;
     } else {
-		out_char(tty->console, nr_tty + '0');
-		out_char(tty->console, '#');
+        out_char(tty->console, nr_tty + '0');
+        out_char(tty->console, '#');
     }
 
-	set_cursor(tty->console->cursor);
-}
-
-static void set_video_start_addr(u32 addr) {
-    disable_interrupt();
-	out_byte(CRTC_ADDR_REG, START_ADDR_H);
-	out_byte(CRTC_DATA_REG, (addr >> 8) & 0xFF);
-	out_byte(CRTC_ADDR_REG, START_ADDR_L);
-	out_byte(CRTC_DATA_REG, addr & 0xFF);
-    enable_interrupt();
+    set_cursor(tty->console->cursor);
 }
 
 void select_console(int nr_console) {
@@ -78,23 +109,21 @@ void select_console(int nr_console) {
 
     g_current_console = nr_console;
 
-    set_video_start_addr(g_console_table[nr_console].current_start_addr);
-    set_cursor(g_console_table[nr_console].cursor);
+    flush(&g_console_table[nr_console]);
 }
 
 void scroll_screen(console_t* console, int direction) {
-	if (direction == SCR_UP) {
-		if (console->current_start_addr > console->original_addr) {
-			console->current_start_addr -= SCREEN_WIDTH;
-		}
-	} else if (direction == SCR_DN) {
-		if (console->current_start_addr + SCREEN_SIZE <
-		    console->original_addr + console->v_mem_limit) {
-			console->current_start_addr += SCREEN_WIDTH;
-		}
-	} else{
-	}
+    if (direction == SCR_UP) {
+        if (console->current_start_addr > console->original_addr) {
+            console->current_start_addr -= SCREEN_WIDTH;
+        }
+    } else if (direction == SCR_DN) {
+        if (console->current_start_addr + SCREEN_SIZE <
+            console->original_addr + console->v_mem_limit) {
+            console->current_start_addr += SCREEN_WIDTH;
+        }
+    } else{
+    }
 
-	set_video_start_addr(console->current_start_addr);
-	set_cursor(console->cursor);
+    flush(console);
 }
--- project/kernel/tty.c
+++ project/kernel/tty.c
@@ -63,6 +63,17 @@ void task_tty(void) {
     }
 }
 
+static void put_key(tty_t* tty, u32 key) {
+	if (tty->count < TTY_IN_BYTES) {
+		*(tty->head) = key;
+		tty->head++;
+		if (tty->head == tty->buf + TTY_IN_BYTES) {
+			tty->head = tty->buf;
+		}
+		tty->count++;
+	}
+}
+
 void in_process(tty_t* tty, u32 key) {
     char output[2] = {'\0', '\0'};
 
@@ -78,6 +89,12 @@ void in_process(tty_t* tty, u32 key) {
     } else {
         int raw_code = key & MASK_RAW;
         switch(raw_code) {
+        case ENTER:
+			put_key(tty, '\n');
+            break;
+        case BACKSPACE:
+			put_key(tty, '\b');
+            break;
         case UP:
             if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R)) {
                 scroll_screen(tty->console, SCR_DN);
